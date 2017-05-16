62794ff9348b2cc187a6e5b57ca88c6dfa3d59e6
ZQian <qian.zhang01@hand-china.com>
2017-05-16 13:57:51 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -2,19 +2,20 @@
 FROM marcielmj/java-8-alpine
 
 #坐着
-MAINTAINER ZQiannnn,604922962@qq.com
+MAINTAINER ZQiannnn,<604922962@qq.com>
 
 #执行命令
 RUN apk add --update libstdc++ && \
     rm -rf /etc/apk/keys/sgerrand.rsa.pub /tmp/* /var/cache/apk/*
-#安装一些软件
+
+#安装一些软件 git
 #RUN
 
 #复制文件到镜像内
 #拷贝Hybris源代码
-ADD binaries/hybris/bin /opt/
+ADD binaries/ /opt/
 
-#ADD aspects /opt/aspects
+ADD aspects /opt/aspects
 
 #拷贝执行脚本
 ADD startup.sh /opt/startup/
@@ -29,7 +30,8 @@ RUN chmod +x /opt/startup/startup.sh && \
 
 ENV CATALINA_SECURITY_OPTS=-Djava.security.egd=file:/dev/./urandom
 ENV CATALINA_MEMORY_OPTS=-Xms2G\ -Xmx2G
-ENV HTTPS_PORT=8088
+ENV HTTP_PORT=9001
+ENV HTTPS_PORT=9002
 ENV AJP_PORT=8009
 ENV KEYSTORE_LOCATION=/etc/ssl/certs/hybris/keystore
 ENV KEYSTORE_PASSWORD=123456
--- startup.sh
+++ startup.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 set -e
+#设置tomcat运行参数
 export CATALINA_OPTS="-DPLATFORM_HOME=$PLATFORM_HOME $CATALINA_SECURITY_OPTS $CATALINA_MEMORY_OPTS -Dorg.apache.tomcat.util.digester.PROPERTY_SOURCE=de.hybris.tomcat.EnvPropertySource $ADDITIONAL_CATALINA_OPTS"
+#设置切面，没有的话用default
 export ASPECT_NAME=${1:-default}
-export CATALINA_BASE="/opt/aspects/$ASPECT_NAME/tomcat"
+
+#设置tomcat工作目录  只启动一个tomcat实例
+export CATALINA_BASE="/opt/aspects/tomcat"
+
+#以aspect来控制tomcat的conf
 export HYBRIS_OPT_CONFIG_DIR="/opt/aspects/$ASPECT_NAME/hybris/conf"
+
+#等待端口打开
 yWaitForPort.sh $WAIT_FOR
 
+
+#jvm route   负载均衡使用
 if [ "$JVM_ROUTE" == "dynamic" ]; then
 	export JVM_ROUTE="$(yGenerateUUID.sh)"
 	echo "Dynamic jvmRoute has been requested. Using: $JVM_ROUTE"
 fi
 
+#admin aspect  用来执行ant命令 第二个参数为命令  需要在宿主机执行 若Jenkins独立的话  如何
 if [ "$ASPECT_NAME" == "admin" ]; then
 	# we use cd because setantenv.sh uses `pwd` to export PLATFORM_HOME
 	cd ${PLATFORM_HOME} && source setantenv.sh
