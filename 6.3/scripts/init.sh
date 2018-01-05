cca7a40fd03bfd53e49f17afaa7c15e6535cb482
ZQian <604922962@qq.com>
2018-01-05 15:40:47 +0800

    fix cluster

--- 6.3/scripts/init.sh
+++ 6.3/scripts/init.sh
@@ -3,8 +3,9 @@ source code.sh
 source config.sh
 
 export DOCKER_CONTAINER_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-echo "
-cluster.broadcast.method.jgroups.tcp.bind_addr=$DOCKER_CONTAINER_IP" >> /opt/hybris/config/local.properties
+sed -i "s/cluster.broadcast.method.jgroups.tcp.bind_addr=127.0.0.1/cluster.broadcast.method.jgroups.tcp.bind_addr=$DOCKER_CONTAINER_IP/g" /opt/hybris/bin/platform/project.properties
+sed -i 's/clustermode=false/clustermode=true/g' /opt/hybris/bin/platform/project.properties
+sed -i 's/cluster.nodes.autodiscovery=false/cluster.nodes.autodiscovery=true/g' /opt/hybris/bin/platform/project.properties
 
 source clean.sh
 
