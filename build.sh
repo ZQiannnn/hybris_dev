f97c9bec9471acfb077062327bfdc56f9757a2d3
ZQian <604922962@qq.com>
2018-01-06 11:52:57 +0800

    fix cluster

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -t zqiannnn/hybris-dev:1.0 .
+docker build -t  registry.cn-hangzhou.aliyuncs.com/hybris-images/hybris-dev:1.0 .
+docker push registry.cn-hangzhou.aliyuncs.com/hybris-images/hybris-dev:1.0
