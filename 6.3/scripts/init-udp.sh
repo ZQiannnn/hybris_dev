14bef3e7e59df137c5ddaa3dfa1c409536a419fa
ZQian <604922962@qq.com>
2018-03-03 15:59:17 +0800

    fix cluster

--- 6.3/scripts/init-udp.sh
+++ 6.3/scripts/init-udp.sh
@@ -5,11 +5,9 @@ source binaries.sh
 source code.sh
 source config.sh
 
-export DOCKER_CONTAINER_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-#sed -i "s/cluster.broadcast.method.jgroups.tcp.bind_addr=127.0.0.1/cluster.broadcast.method.jgroups.tcp.bind_addr=$DOCKER_CONTAINER_IP/g" /opt/hybris/bin/platform/project.properties
 sed -i 's/clustermode=false/clustermode=true/g' /opt/hybris/bin/platform/project.properties
 sed -i 's/cluster.nodes.autodiscovery=false/cluster.nodes.autodiscovery=true/g' /opt/hybris/bin/platform/project.properties
-#sed -i 's/cluster.broadcast.method.jgroups.configuration=jgroups-udp.xml/cluster.broadcast.method.jgroups.configuration=jgroups-tcp.xml/g' /opt/hybris/bin/platform/project.properties
+sed -i 's/cluster.broadcast.method.jgroups.configuration=jgroups-udp.xml/cluster.broadcast.method.jgroups.configuration=/opt/hybris/jgroups/jgroups-udp.xml/g' /opt/hybris/bin/platform/project.properties
 
 source clean.sh
 
