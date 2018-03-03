e746316098b4571715bdf61275c67c233c10842d
ZQian <604922962@qq.com>
2018-03-03 16:09:01 +0800

    fix cluster

--- 6.3/build.sh
+++ 6.3/build.sh
@@ -1,2 +1,4 @@
 docker build -t registry.cn-hangzhou.aliyuncs.com/hybris-images/hybris-dev:hybris-6.3_v1 .
 docker push registry.cn-hangzhou.aliyuncs.com/hybris-images/hybris-dev:hybris-6.3_v1
+docker tag registry.cn-hangzhou.aliyuncs.com/hybris-images/hybris-dev:hybris-6.3_v1 zqiannnn/hybris-dev:hybris-6.3_v1
+docker push zqiannnn/hybris-dev:hybris-6.3_v1
